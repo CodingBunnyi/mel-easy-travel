1da129a389134a59ed580b410e7a6cda6468f730
Snow <yh984980263@gmail.com>
2022-10-12 23:03:00 +0800

    api add param

--- backend/server.py
+++ backend/server.py
@@ -1,5 +1,5 @@
 # Import flask and datetime module for showing date and time
-from flask import Flask
+from flask import Flask, request
 
 import datetime
   
@@ -10,16 +10,12 @@ app = Flask(__name__)
   
   
 # Route for seeing a data
-@app.route('/data')
+@app.route('/data', methods=['get'])
 def get_time():
-  
-    # Returning an api for showing in  reactjs
-    return {
-        'Name':"geek", 
-        "Age":"22",
-        "Date":x, 
-        "programming":"python"
-        }
+    # Returning an api for showing in  reactj
+    hello = request.args.get('hello', None)
+    world = request.args.get('world', None)
+    return f'{hello}...{world}'
   
       
 # Running app
--- src/utils/twitterDataApi.js
+++ src/utils/twitterDataApi.js
@@ -1,5 +1,9 @@
 import axios from 'axios';
 
 export const getTwitterData = axios.create({
-  baseURL: 'http://localhost:3000/data'
+  baseURL: 'http://localhost:3000/data',
+  params: {
+    hello:'1',
+    world:'2'
+  }
 });
