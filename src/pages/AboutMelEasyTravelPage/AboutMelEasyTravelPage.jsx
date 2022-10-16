ba0b4a566a3d5c54524e72fe105cd0fc6f3217f4
Peicong Shangguan <pshanggu@gmail.com>
2022-10-17 05:29:49 +0800

    a

--- src/pages/AboutMelEasyTravelPage/AboutMelEasyTravelPage.jsx
+++ src/pages/AboutMelEasyTravelPage/AboutMelEasyTravelPage.jsx
@@ -1,8 +1,35 @@
 import React from 'react';
 import './AboutMelEasyTravelPage.scss'
 const AboutMelEasyTravelPage = () => (
-  <div className="page">
-    This is About MelEasyTravel page
+  <div className="card">
+    <h1 className="h1">About us</h1>
+    <p1 className="p1">MelEasyTravel is xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx<br></br>xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx<br></br>xxxxxxxxxxxxxxxxxxx.</p1>
+    <h2 className="h2">Contact us</h2>
+    
+    <div className="card1">
+      <h3 className="h3">Programmer</h3>
+      <p2 className="p2">PEICONG SHANGGUAN<br></br>pshangguan@gmail.com</p2>
+
+    </div>
+
+    <div className="card2">
+      <h3 className="h3">Data analysis</h3>
+      <p2 className="p2">PEICONG SHANGGUAN<br></br>pshangguan@gmail.com</p2>
+
+    </div>
+
+    <div className="card3">
+      <h3 className="h3">Programmer</h3>
+      <p2 className="p2">PEICONG SHANGGUAN<br></br>pshangguan@gmail.com</p2>
+
+    </div>
+
+    <div className="card4">
+      <h3 className="h3">Programmer</h3>
+      <p2 className="p2">PEICONG SHANGGUAN<br></br>pshangguan@gmail.com</p2>
+
+    </div>
+
   </div>
 )
 
