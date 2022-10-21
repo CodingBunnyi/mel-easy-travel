2f4ddac9ce6862c5dd317f48d59d3eedaa648563
Xiaoxi Zhang (Tessie) <xiaoxzhang2@student.unimelb.edu.au>
2022-10-22 03:48:44 +1100

    Add icon marks & Resources

--- src/pages/MapPage/components/MelCityMap/components/ChurchPin.jsx
+++ src/pages/MapPage/components/MelCityMap/components/ChurchPin.jsx
@@ -4,56 +4,84 @@ import { pointer } from 'd3-selection';
 import React, {useMemo} from 'react';
 import church from '../../../../../assets/images/church.svg';
 import college from '../../../../../assets/images/college.svg';
-
+import metro from '../../../../../assets/images/metro.svg';
+import outdoor from '../../../../../assets/images/outdoor.svg';
+import police from '../../../../../assets/images/police.svg';
+import publicBuilding from '../../../../../assets/images/publicBuilding.svg';
+import guest from '../../../../../assets/images/guest.svg';
+import cemetery from '../../../../../assets/images/cemetery.svg';
+import sport from '../../../../../assets/images/sport.svg';
+import museum from '../../../../../assets/images/museum.svg';
+import boat from '../../../../../assets/images/boat.svg';
+import bridge from '../../../../../assets/images/bridge.svg';
+import casino from '../../../../../assets/images/casino.svg';
+import cinema from '../../../../../assets/images/cinema.svg';
+import construction from '../../../../../assets/images/construction.svg';
+import exhi from '../../../../../assets/images/exhi.svg';
+import factory from '../../../../../assets/images/factory.svg';
+import fire from '../../../../../assets/images/fire.svg';
+import fish from '../../../../../assets/images/fish.svg';
+import gym from '../../../../../assets/images/gym.svg';
+import hospital from '../../../../../assets/images/hospital.svg';
+import hotel from '../../../../../assets/images/hotel.svg';
+import house from '../../../../../assets/images/house.svg';
+import land from '../../../../../assets/images/land.svg';
+import store from '../../../../../assets/images/store.svg';
+import threatre from '../../../../../assets/images/threatre.svg';
+import retail from '../../../../../assets/images/retail.svg';
+import squre from '../../../../../assets/images/squre.svg';
+import tower from '../../../../../assets/images/tower.svg';
+import indoor from '../../../../../assets/images/indoor.svg';
+import yard from '../../../../../assets/images/yard.svg';
+import synagogue from '../../../../../assets/images/synagogue.svg';
 
 const data = {
-  "Railway Station": college, 
-  "Retail/Office/Carpark": college, 
-  "Informal Outdoor Facility (Park/Garden/Reserve)": church, 
+  "Railway Station": metro, 
+  "Retail/Office/Carpark": publicBuilding, 
+  "Informal Outdoor Facility (Park/Garden/Reserve)": outdoor, 
   "Church": church, 
-  "Private Hospital": church, 
-  "Synagogue": church, 
-  "Police Station": church, 
-  "Visitor Centre": church, 
-  "Major Sports &amp; Recreation Facility": church, 
-  "Cemetery": church, 
-  "Retail": church, 
-  "Primary Schools": church, 
-  "Art Gallery/Museum": church, 
-  // {name: "Secondary Schools", icon: church}, 
-  // {name: "School - Primary and Secondary Education", icon: church}, 
-  // {name: "Theatre Live", icon: church}, 
-  // {name: "Tertiary (University)", icon: church}, 
-  // {name: "Public Buildings", icon: church}, 
-  // {name: "Retail/Office", icon: church}, 
-  // {name: "Office", icon: church}, 
-  // {name: "Government Building", icon: church}, 
-  // {name: "Outdoor Recreation Facility (Zoo, Golf Course)", icon: church}, 
-  // {name: "Function/Conference/Exhibition Centre", icon: church}, 
-  // {name: "Vacant Land - Undeveloped Site", icon: church}, 
-  // {name: "Casino", icon: church}, 
-  // {name: "Public Hospital", icon: church}, 
-  // {name: "Film &amp; RV Studio", icon: church}, 
-  // {name: "Current Construction Site", icon: church}, 
-  // {name: "Marina", icon: church}, 
-  // {name: "Observation Tower/Wheel", icon: church}, 
-  // {name: "Further Education", icon: church}, 
-  // {name: "Hostel", icon: church}, 
-  // {name: "Current Construction Site - Commercial", icon: church}, 
-  // {name: "Cinema", icon: church}, 
-  // {name: "Indoor Recreation Facility", icon: church}, 
-  // {name: "Dwelling (House)", icon: church}, 
-  // {name: "Industrial (Manufacturing)", icon: church}, 
-  // {name: "Store Yard", icon: church}, 
-  // {name: "Fire Station", icon: church}, 
-  // {name: "Retail/Office/Residential/Carpark", icon: church}, 
-  // {name: "Gymnasium/Health Club", icon: church}, 
-  // {name: "Aquarium", icon: church}, 
-  // {name: "Retail/Residential", icon: church}, 
-  // {name: "Bridge", icon: church}, 
-  // {name: "Department Store", icon: church}, 
-  // {name: "Medical Services", icon: church}, 
-   
+  "Private Hospital": hospital, 
+  "Synagogue": synagogue, 
+  "Police Station": police, 
+  "Visitor Centre": guest, 
+  "Major Sports &amp; Recreation Facility": sport, 
+  "Cemetery": cemetery, 
+  "Retail": retail, 
+  "Primary Schools": college, 
+  "Art Gallery/Museum": museum, 
+  "Secondary Schools": college, 
+  "School - Primary and Secondary Education": college, 
+  "Theatre Live": threatre, 
+  "Tertiary (University)": college, 
+  "Public Buildings": publicBuilding, 
+  "Retail/Office": retail, 
+  "Office": publicBuilding, 
+  "Government Building": publicBuilding, 
+  "Outdoor Recreation Facility (Zoo, Golf Course)": squre, 
+  "Function/Conference/Exhibition Centre": exhi, 
+  "Vacant Land - Undeveloped Site": land, 
+  "Casino": casino, 
+  "Public Hospital": hospital, 
+  "Film &amp; RV Studio": cinema, 
+  "Current Construction Site": construction, 
+  "Marina": boat, 
+  "Observation Tower/Wheel": tower, 
+  "Further Education": college, 
+  "Hostel": hotel, 
+  "Current Construction Site - Commercial": construction, 
+  "Cinema": cinema, 
+  "Indoor Recreation Facility": indoor, 
+  "Dwelling (House)": house, 
+  "Industrial (Manufacturing)": factory, 
+  "Store Yard": yard, 
+  "Fire Station": fire, 
+  "Retail/Office/Residential/Carpark": retail, 
+  "Gymnasium/Health Club": gym, 
+  "Aquarium": fish, 
+  "Retail/Residential": retail, 
+  "Bridge": bridge, 
+  "Department Store": store, 
+  "Medical Services": hospital, 
 };
   
 function ChurchPin({ poi }) {
@@ -61,7 +89,7 @@ function ChurchPin({ poi }) {
   const customPins = useMemo(
     () =>      
       <img 
-        height={ 20 }
+        height={ 25 }
         style={ { 'cursor': 'pointer'} }
         key= { poi.SubTheme }
         src={ data[poi.SubTheme] }
