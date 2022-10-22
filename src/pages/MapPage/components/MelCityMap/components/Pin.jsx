54dff96df48652abdc5f12f7e1b9625e3e1907cb
Snow <yh984980263@gmail.com>
2022-10-22 19:28:51 +0800

    change icon

--- src/pages/MapPage/components/MelCityMap/components/Pin.jsx
+++ src/pages/MapPage/components/MelCityMap/components/Pin.jsx
@@ -1,10 +1,10 @@
 import * as React from 'react';
 import FiberManualRecordIcon from '@mui/icons-material/FiberManualRecord';
-import { pink } from '@mui/material/colors';
+import { indigo } from '@mui/material/colors';
 
 function Pin() {
   return (
-    <FiberManualRecordIcon style={ { 'cursor': 'pointer'} } sx={ { color: pink[300], fontSize: 7 } }/>
+    <FiberManualRecordIcon style={ { 'cursor': 'pointer'} } sx={ { color: indigo [300], fontSize: 7 } }/>
   );
 }
 
